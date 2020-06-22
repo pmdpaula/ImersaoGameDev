6231789b552560e0a24ce4597bc8657066c42558
Pedro de Paula <pmdpaula@yahoo.com.br>
2020-06-22 15:03:55 -0300

    Cenário rodando ao fundo.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,57 @@
+
+let imagemCenario;
+let imagemPersonagem;
+let personagemAltura = 160;
+let personagemLargura = Math.round((personagemAltura * (1 - 0.2272) ));
+
+// personagemLargura
+
+function preload() {
+  imagemCenario = loadImage('imagens/cenario/floresta.png');
+  imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+  
+}
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  cenario = new Cenario(imagemCenario, 3);
+  // frameRate(40);
+
 }
 
 function draw() {
-  background(220);
+  cenario.exibe();
+  cenario.move();
+  image(imagemPersonagem, 0, height - personagemAltura, personagemLargura, personagemAltura, 0, 0, 220, 270);
+  circle(mouseX, mouseY, 40);
+}
+
+
+class Cenario {
+  constructor(imagem, velocidade) {
+    this.imagem = imagem;
+    this.velocidade = velocidade;
+    this.x1 = 0;
+    this.x2 = width;
+  }
+
+  exibe() {
+    image(this.imagem, this.x1, 0, width, height);
+    image(this.imagem, this.x2, 0, width, height);  
+  }
+
+  move() {
+    this.x1 = this.x1 - this.velocidade;
+    this.x2 = this.x2 - this.velocidade;
+
+    if ( this.x1 < -width ) {
+      this.x1 = width;
+    }
+
+    if ( this.x2 < -width ) {
+      this.x2 = width;
+    }
+
+  }
 }
